2d74d57cc9102c012e003cceda426b3c2b3c2a4e
Mimrpim <mimrpim@gmail.com>
2026-01-20 17:18:51 +0100

    added project

---
+++ global_var.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var player_movement = true
+var is_player_dead = false
+var player_health = 3
+var player_max_health = 3
+const house_01_scene_in = "res://scenes/levels/examples/01_house_in.tscn"
+const out_01 = "res://scenes/levels/examples/01.tscn"
+var hide_overaly = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if player_health == 0:
+		is_player_dead = true
+	else:
+		is_player_dead = false
+	if is_player_dead:
+		player_movement = false
+	else:
+		player_movement = true
