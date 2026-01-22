1be0990ed47d0b99b43c53c5527c7b2da5797796
Mimrpim <mimrpim@gmail.com>
2026-01-22 17:37:07 +0100

    added secret
    added secret 1 and code (no animation

---
+++ scenes/levels/tutorial/secret_01.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var timer: Timer = $AudioStreamPlayer/Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	timer.wait_time = 0.5
+	timer.start()
+	global_var.player_movement = false
+	$"../Player".set_position(Vector2(1321, 119))
+
+func _on_timer_timeout() -> void:
+	if timer.wait_time == 0.5:
+		timer.wait_time = 4.20
+		timer.start()
+		audio_stream_player.play()
+		$"../TileMapLayer/TileMapLayer2".hide()
+	else:
+		audio_stream_player.stop()
