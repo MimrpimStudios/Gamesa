6c8c6f8413700b776888570da35b188df63eeafd
Mimrpim <mimrpim@gmail.com>
2026-01-25 13:01:34 +0100

    added debug overaly

---
+++ scenes/debug.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var label: Label = $LabelVersion
+@onready var label_fps: Label = $LabelFPS
+@export var version: String
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	label.text = "Gamesa version: " + ProjectSettings.get_setting("application/config/version") + " or version: " + version
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	label_fps.text = "FPS: " + str(Engine.get_frames_per_second())
