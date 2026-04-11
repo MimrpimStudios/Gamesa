f44ea6280b8966dad45ec27c5a68eeb4541d1612
Mimrpim <mimrpim@gmail.com>
2026-04-11 18:50:58 +0200

    sync

--- scripts/rat_smash_platform.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatableBody2D
-
-@onready var collision: CollisionShape2D = $SmashJump/CollisionShape2D
-
-func _physics_process(_delta: float) -> void:
-	if global_var.is_player_smashing:
-		collision.disabled = false
-	else:
-		collision.disabled = true
