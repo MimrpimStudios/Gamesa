3ef85d3ef8930808f59dc8561e133d272226ab05
Mimrpim <mimrpim@gmail.com>
2026-05-11 17:16:26 +0200

    added stop for sewers music

---
+++ scenes/levels/tutorial/01_boss_1.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	global_var.player_movement = true
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	SewersMusic.stop()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_released("esc"):
+		get_tree().change_scene_to_file(global_var.main_menu_scene)
