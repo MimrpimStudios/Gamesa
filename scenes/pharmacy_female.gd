b3a3a4168360603420e691d3393a8fb597a8b62d
Mimrpim <mimrpim@gmail.com>
2026-05-26 18:52:48 +0200

    added physic to npcs

---
+++ scenes/pharmacy_female.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+@export_range(0, 100, 0.1, "or_greater", "or_less") var SPEED: float = 150.0
+@export_range(0, 1000, 1.0, "or_greater", "or_less") var JUMP_VELOCITY_kladna = 450.0
+var JUMP_VELOCITY = JUMP_VELOCITY_kladna * -1
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+
+	move_and_slide()
