0e2ce8edf3df7586081996a4105706144a07eb51
Mimrpim <mimrpim@gmail.com>
2026-05-30 13:54:55 +0200

    removed the background

--- scripts/levels/tutorial/npc_speak.gd
+++ scripts/levels/tutorial/npc_speak.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-
 @onready var npc: CharacterBody2D = $"../PharmacyMale"
 @onready var animation: AnimationPlayer = $"../AnimationPlayer"
 var speak = false
 var played = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
