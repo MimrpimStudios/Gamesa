53bfac9020d7acf74d9f24c26e46cccfe8c8292d
Mimrpim <mimrpim@gmail.com>
2026-05-30 13:56:28 +0200

    Revert "removed the background"
    This reverts commit 0e2ce8edf3df7586081996a4105706144a07eb51.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,6 @@
 extends Control
 
 @onready var timer: Timer = $Timer
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_launcher_info()
@@ -19,7 +17,6 @@ func _ready() -> void:
 
 	timer.start()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -34,17 +31,17 @@ func get_launcher_info():
 	print("=".repeat(30))
 	var args = OS.get_cmdline_args()
 	var args_user = OS.get_cmdline_user_args()
-
+	
 	print("arguments: " + str(args))
 	print("user arguments: " + str(args_user))
-
+	
 	if args.has("-launcherCLI"):
 		print("-".repeat(30))
 		print("Spusteno pres CLI launcher.")
 		global_var.launcher_type = "CLI"
-
+		
 		var version_value = get_arg_value(args, "-versionCLI")
-
+		
 		if version_value != "":
 			print("CLI version: " + version_value)
 			global_var.launcher_version = version_value
@@ -54,18 +51,17 @@ func get_launcher_info():
 		print("Spusteno pres GUI launcher.")
 		global_var.launcher_type = "GUI"
 		var version_value_gui = get_arg_value(args, "-versionGUI")
-
+		
 		if version_value_gui != "":
 			print("GUI version: " + version_value_gui)
 			global_var.launcher_version = version_value_gui
-
+		
 		print("-".repeat(30))
-
+		
 	print("=".repeat(30))
-
 
 func get_arg_value(argument_list: PackedStringArray, prefix: String) -> String:
 	for arg in argument_list:
 		if arg.begins_with(prefix + "="):
-			return arg.split("=")[1]
+			return arg.split("=")[1] 
 	return ""
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,6 @@ extends Control
 @onready var texture_button_play: TextureButton = $TextureButtonPlay
 @onready var texture_button_exit: TextureButton = $TextureButtonExit
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -14,7 +13,6 @@ func _ready() -> void:
 	if global_var.load_level() == global_var.start_scene_story:
 		continue_game_button.set_disabled(true)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -22,7 +20,8 @@ func _process(_delta: float) -> void:
 
 func _on_texture_button_play_pressed() -> void:
 	panel_show()
-
+	
+	
 	# Old use
 	# global_var.level = global_var.load_level()
 	# print(global_var.level)
@@ -35,13 +34,13 @@ func _on_texture_button_exit_pressed() -> void:
 
 func _on_new_game_button_pressed() -> void:
 	global_var.level = global_var.start_scene_story
-	print("Going to: " + str(global_var.level))
+	print("Going to: "+str(global_var.level))
 	get_tree().change_scene_to_file(global_var.level)
 
 
 func _on_continue_game_button_pressed() -> void:
 	global_var.level = global_var.load_level()
-	print("Going to: " + str(global_var.level))
+	print("Going to: "+str(global_var.level))
 	get_tree().change_scene_to_file(global_var.level)
 
 
@@ -53,7 +52,6 @@ func panel_show():
 	texture_button_exit.hide()
 	texture_button_play.hide()
 	game_choose_panel.show()
-
 
 func panel_hide():
 	texture_button_exit.show()
