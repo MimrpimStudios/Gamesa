9dfcb3524e6c8287ff57ab7aa71abae39632c3f8
Mimrpim <mimrpim@gmail.com>
2026-06-01 19:18:59 +0200

    added block for doors before talking with NPC

--- scripts/levels/tutorial/door_pharmacy_out.gd
+++ scripts/levels/tutorial/door_pharmacy_out.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var repeat4 = false
+@export var work: bool = false
 
 
 func _on_body_exited(_body: Node2D) -> void:
@@ -12,5 +13,5 @@ func _on_body_entered(_body: Node2D) -> void:
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("integrate") and repeat4:
+	if Input.is_action_just_pressed("integrate") and repeat4 and work:
 		get_tree().change_scene_to_file(global_var.town_02_back_home_scene)
