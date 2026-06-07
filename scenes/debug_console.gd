8dda24c51ee7470978ad5dae49bf9bac115b6503
Mimrpim <mimrpim@gmail.com>
2026-06-07 18:55:41 +0200

    added speed and godmode

--- scenes/debug_console.gd
+++ scenes/debug_console.gd
@@ -4,36 +4,43 @@ var shown: bool = false
 var commands = [
 	"help",
 	"clear",
-	"speed"
+	"speed",
+	"god"
 ]
 var syntax = [
 	" - Shows all avalable commands",
 	" - Clears console",
-	" <set|reset> <number> - Sets the player speed (good for need to go fast)"
+	" <set|reset> <float> - Sets the player speed (good for need to go fast)",
+	" - Enable god mode (can not turn off, only by killikng itself)"
 ]
 @onready var root: CanvasLayer = $"."
-@onready var label: Label = $Panel/Label
-@onready var rich_text_label: RichTextLabel = $Panel/RichTextLabel
-@onready var line_edit: LineEdit = $Panel/LineEdit
+@onready var label: Label = $UI_Container/Panel/Label
+@onready var rich_text_label: RichTextLabel = $UI_Container/Panel/RichTextLabel
+@onready var line_edit: LineEdit = $UI_Container/Panel/LineEdit
+@onready var ui_container: Control = $UI_Container
+@onready var panel: Panel = $UI_Container/Panel
 
 var player = null
 var mouse_visible: bool = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-		mouse_visible = true
-	
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		mouse_visible = true
+	else:
+		mouse_visible = false
 	if shown:
 		root.show()
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		global_var.player_movement = false
 	else:
 		root.hide()
-		if not mouse_visible:
+		if mouse_visible == false:
 			Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-
+		global_var.player_movement = true
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("console"):
@@ -64,7 +71,7 @@ func _on_line_edit_text_submitted(text: String) -> void:
 		rich_text_label.text = ""
 	elif command == "speed":
 		line_edit.text = ""
-		player = get_tree().get_first_node_in_group("hrac")
+		player = get_tree().get_first_node_in_group("player") as CharacterBody2D
 		if arg1 == "reset":
 			if player:
 				player.SPEED = 150.0
@@ -72,5 +79,21 @@ func _on_line_edit_text_submitted(text: String) -> void:
 			else:
 				rich_text_label.append_text("Player does not exist" + "\n")
 				
+		elif arg1 == "set":
+			arg2 = float(arg2)
+			if arg2 is float:
+				if player:
+					player.SPEED = arg2
+					rich_text_label.append_text("Player speed is now " + str(player.SPEED) + "\n")
+				else:
+					rich_text_label.append_text("Player does not exist" + "\n")
+			else:
+				rich_text_label.append_text("Syntax: " + commands[2] + syntax[2] + "\n")
+
 		else:
 			rich_text_label.append_text("Syntax: " + commands[2] + syntax[2] + "\n")
+	elif command == "god":
+		line_edit.text = ""
+		global_var.player_max_health = 999
+		global_var.player_health = 999
+		rich_text_label.append_text("Godmode on "+ "\n")
