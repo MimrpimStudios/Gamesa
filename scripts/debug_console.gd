2be38bc340c693169a179ce88ff8ed14e6b22de7
Mimrpim <mimrpim@gmail.com>
2026-06-08 14:20:13 +0200

    fixed player_movement fix

--- scripts/debug_console.gd
+++ scripts/debug_console.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
-var shown: bool = false
+var is_open: bool = false
+
 var commands = [
 	"help",
 	"clear",
@@ -8,11 +9,12 @@ var commands = [
 	"god"
 ]
 var syntax = [
-	" - Shows all avalable commands",
+	" - Shows all available commands",
 	" - Clears console",
-	" <set|reset> <float> - Sets the player speed (good for need to go fast)",
-	" - Enable god mode (can not turn off, only by killikng itself)"
+	" <set|reset> <float> - Sets the player speed",
+	" - Enable god mode (can not turn off, only by killing itself)"
 ]
+
 @onready var root: CanvasLayer = $"."
 @onready var label: Label = $UI_Container/Panel/Label
 @onready var rich_text_label: RichTextLabel = $UI_Container/Panel/RichTextLabel
@@ -21,85 +23,70 @@ var syntax = [
 @onready var panel: Panel = $UI_Container/Panel
 
 var player = null
-var mouse_visible: bool = false
-var showed: bool = false
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+# Proměnná pro uložení původního režimu myši před otevřením konzole
+var PREVIOUS_MOUSE_MODE = Input.MOUSE_MODE_CAPTURED 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-		mouse_visible = true
-	else:
-		mouse_visible = false
-	if shown:
-		root.show()
-		if not showed:
-			showed = true
-			line_edit.grab_focus()
-			line_edit.clear()
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		global_var.player_movement = false
-	else:
-		showed = false
-		root.hide()
-		if mouse_visible == false:
-			Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-		global_var.player_movement = true
+func _ready() -> void:
+	root.hide() # Na začátku hru startujeme se skrytou konzolí
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("console"):
-		if shown:
-			shown = false
+		is_open = !is_open # Prohodí true/false
+		
+		if is_open:
+			# OTEVŘENÍ KONZOLE
+			PREVIOUS_MOUSE_MODE = Input.mouse_mode # Uložíme si, jak to měl hráč předtím
+			root.show()
+			line_edit.clear()
+			line_edit.grab_focus()
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			global_var.player_movement = false
 		else:
-			shown = true
-
+			# ZAVŘENÍ KONZOLE
+			root.hide()
+			Input.mouse_mode = PREVIOUS_MOUSE_MODE # Vrátíme myš do původního stavu
+			global_var.player_movement = true
 
 func _on_line_edit_text_submitted(text: String) -> void:
-	line_edit.grab_focus()
-	line_edit.clear()
+	text = text.strip_edges() # Odstraní nechtěné mezery na začátku/konci
+	if text == "":
+		return
+		
 	var command = text.get_slice(" ", 0)
 	var arg1 = text.get_slice(" ", 1)
 	var arg2 = text.get_slice(" ", 2)
 
 	if not command in commands:
 		rich_text_label.append_text("Invalid command " + command + "\n")
-		rich_text_label.append_text("Type help for list of commands" + "\n")
+		rich_text_label.append_text("Type help for list of commands\n")
+		
 	elif command == "help":
-		rich_text_label.append_text("Avalable commands: \n\n")
-		var counter_commands: int = 0
-		for item in commands:
-			rich_text_label.append_text(item + syntax[counter_commands] + "\n")
-			counter_commands += 1
+		rich_text_label.append_text("Available commands: \n")
+		for i in range(commands.size()):
+			rich_text_label.append_text(commands[i] + syntax[i] + "\n")
+			
 	elif command == "clear":
 		rich_text_label.clear()
+		
 	elif command == "speed":
 		player = get_tree().get_first_node_in_group("player") as CharacterBody2D
-		if arg1 == "reset":
-			if player:
-				player.SPEED = 150.0
-				rich_text_label.append_text("Player speed is now " + str(player.SPEED) + "\n")
-			else:
-				rich_text_label.append_text("Player does not exist" + "\n")
-				
-		elif arg1 == "set":
-			arg2 = float(arg2)
-			if arg2 is float:
-				if player:
-					player.SPEED = arg2
-					rich_text_label.append_text("Player speed is now " + str(player.SPEED) + "\n")
-				else:
-					rich_text_label.append_text("Player does not exist" + "\n")
-			else:
-				rich_text_label.append_text("Syntax: " + commands[2] + syntax[2] + "\n")
-
+		
+		if not player:
+			rich_text_label.append_text("Player does not exist\n")
+		elif arg1 == "reset":
+			player.SPEED = 150.0
+			rich_text_label.append_text("Player speed is now " + str(player.SPEED) + "\n")
+		elif arg1 == "set" and arg2.is_valid_float():
+			player.SPEED = float(arg2)
+			rich_text_label.append_text("Player speed is now " + str(player.SPEED) + "\n")
 		else:
 			rich_text_label.append_text("Syntax: " + commands[2] + syntax[2] + "\n")
+			
 	elif command == "god":
-
 		global_var.player_max_health = 999
 		global_var.player_health = 999
-		rich_text_label.append_text("Godmode on "+ "\n")
+		rich_text_label.append_text("Godmode on\n")
 	
+	# Vyčistíme text box a necháme v něm kurzor pro další psaní
+	line_edit.clear()
 	line_edit.call_deferred("grab_focus")
