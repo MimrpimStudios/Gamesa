ed1332aaf6139abbc521fc718eeee6433b786884
Mimrpim <mimrpim@gmail.com>
2026-06-27 13:01:47 +0200

    changed how scene changer work
    now is needed to add collisionshape2D

--- scenes/objects/scene_change.gd
+++ scenes/objects/scene_change.gd
@@ -2,16 +2,13 @@ extends Area2D
 
 ## A scene when is entered into the area
 @export var scene: PackedScene
-## The actual shape owned by this collision shape.
-@export var shape: Shape2D
+
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if not shape == null:
-		collision.set_shape(shape)
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
