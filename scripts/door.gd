231c00d50f0c2523b7821a567b068a5fbcbacdd1
Mimrpim <mimrpim@gmail.com>
2026-06-27 12:26:41 +0200

    added textures

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,13 +3,24 @@ extends Area2D
 
 ## A scene when the door is used. Leave <empty> for disabling them.
 @export var scene: PackedScene
+@export_group("Texture")
+## A texture used when the door is active
+@export var door_texture: Texture2D = preload("uid://cmeo2j124jyhm")
+## A texture when the door is disabled
+@export var door_texture_disabled: Texture2D = preload("uid://byiq3ml1gstxt")
+
 
 var entered: bool = false
+
+@onready var sprite: Sprite2D = $Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if scene == null:
+		sprite.set_texture(door_texture_disabled)
+	else:
+		sprite.set_texture(door_texture)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
