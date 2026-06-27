1b9786bc9ae3543585bd78446e27d70412f36c6a
Mimrpim <mimrpim@gmail.com>
2026-06-27 12:40:38 +0200

    Add scene transition on player collision
    Implement scene change functionality when the player collides with the scene change trigger. This adds a _on_body_entered signal handler that detects player contact and transitions to the configured scene using get_tree().change_scene_to_packed().

--- scenes/objects/scene_change.gd
+++ scenes/objects/scene_change.gd
@@ -16,3 +16,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_packed(scene)
