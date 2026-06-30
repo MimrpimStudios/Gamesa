b47d933f4cb0d2e007d09614015e28044aa7a0ef
Mimrpim <mimrpim@gmail.com>
2026-06-30 12:02:34 +0200

    small bux fixes

--- scripts/levels/tutorial/area_2d2.gd
+++ scripts/levels/tutorial/area_2d2.gd
@@ -2,12 +2,20 @@ extends Area2D
 
 var repeat5 = false
 
+@onready var label: Label = %Label
+
+func _ready() -> void:
+	label.hide()
+
 func _on_body_exited(_body: Node2D) -> void:
 	repeat5 = false
+	label.hide()
 
 
 func _on_body_entered(_body: Node2D) -> void:
 	repeat5 = true
+	label.show()
+
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("integrate") and repeat5:
--- scripts/levels/tutorial/pharmacy-enter.gd
+++ scripts/levels/tutorial/pharmacy-enter.gd
@@ -2,13 +2,19 @@ extends Area2D
 
 var repeat5 = false
 
+@onready var label: Label = %Label
+
+func _ready() -> void:
+	label.hide()
 
 func _on_body_exited(_body: Node2D) -> void:
 	repeat5 = false
+	label.hide()
 
 
 func _on_body_entered(_body: Node2D) -> void:
 	repeat5 = true
+	label.show()
 
 
 func _process(_delta: float) -> void:
