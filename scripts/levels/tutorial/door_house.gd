9244286fc141178c90f89075425348cc0860c8b2
Mimrpim <mimrpim@gmail.com>
2026-07-07 21:17:29 +0200

    const fix conzistency

--- scripts/levels/tutorial/door_house.gd
+++ scripts/levels/tutorial/door_house.gd
@@ -13,4 +13,4 @@ func _on_body_entered(_body: Node2D) -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("integrate") and repeat:
-		get_tree().change_scene_to_file(global_var.house_01_scene_in)
+		get_tree().change_scene_to_file(global_var.house_01_in_scene)
