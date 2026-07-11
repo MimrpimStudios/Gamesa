674e382db84c48c6727032e6e381db6c5080b82b
Mimrpim <mimrpim@gmail.com>
2026-07-11 15:12:00 +0200

    Update main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	get_launcher_info()
 	if global_var.launcher_type != "":
 		if global_var.launcher_type == "GUI":
