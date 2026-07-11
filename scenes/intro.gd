fa0f596dbb8e1f6f56fba0b7f9cc5d09a112f986
Mimrpim <mimrpim@gmail.com>
2026-07-11 14:51:01 +0200

    added skip intro

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -68,3 +68,31 @@ func _process(_delta: float) -> void:
 func _on_audio_stream_player_finished() -> void:
 	audio_stream_player.stream = DRUM_LOOP
 	audio_stream_player.play()
+
+func _input(event):
+	# Zkontrolujeme, jestli jde o stisk klávesy (a ne o pohyb myši apod.)
+	# event.is_pressed() zajistí, že kód proběhne jen při zmáčknutí, ne při puštění klávesy
+	if event is InputEventKey and event.is_pressed():
+		
+		# Zde definujeme seznam fyzických kláves, které chceme ignorovat
+		var ignorovane_klavesy = [
+			KEY_VOLUMEUP,
+			KEY_VOLUMEDOWN,
+			KEY_VOLUMEMUTE,
+			KEY_MEDIANEXT,
+			KEY_MEDIAPREVIOUS,
+			KEY_MEDIAPLAY,
+			KEY_MEDIASTOP
+		]
+		
+		# Pokud je zmáčknutá klávesa v našem seznamu ignorovaných, ukončíme funkci
+		if event.physical_keycode in ignorovane_klavesy:
+			return
+			
+		# ---- TADY UŽ MŮŽEŠ DĚLAT COKOLIV CHCEŠ ----
+		print("Byla zmáčknuta platná klávesa: ", event.as_text())
+		_moje_akce()
+
+func _moje_akce():
+	# Zde bude tvůj kód, který se má spustit
+	get_tree().change_scene_to_file(global_var.main_scene)
