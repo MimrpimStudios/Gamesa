d7fb67917b7fa2d6b9ca24717b7eefe4633d3fee
Mimrpim <mimrpim@gmail.com>
2026-08-17 21:57:37 +0200

    healthbar show

--- scripts/player_healthbar.gd
+++ scripts/player_healthbar.gd
@@ -10,8 +10,7 @@ var player_heart_procent_old
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	visible = global_var.healthbar_show
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
