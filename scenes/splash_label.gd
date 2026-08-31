46cec25c559ecef7a0ab1746671011f52579cb2e
Mimrpim <mimrpim@gmail.com>
2026-08-31 15:49:20 +0200

    fixed bug Invalid call. Nonexistent function 'pick_random' in base 'PackedStringArray'.

--- scenes/splash_label.gd
+++ scenes/splash_label.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	if FileAccess.file_exists(SPLASHES_FILE):
 		var file = FileAccess.open(SPLASHES_FILE, FileAccess.READ)
 		var content = file.get_as_text()
-		var splashes = content.split("\n", false)
+		var splashes = Array(content.split("\n", false))
 		
 		if not splashes.is_empty():
 			text = splashes.pick_random()
