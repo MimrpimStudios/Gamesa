9b134d71e25cddff89414b5e5c79202b7e542bec
Mimrpim <mimrpim@gmail.com>
2026-08-31 15:46:45 +0200

    Update splash_label.gd
    fixed bug of Invalid access of index '21' on a base object of type: 'Array'. on line 14

--- scenes/splash_label.gd
+++ scenes/splash_label.gd
@@ -1,21 +1,12 @@
 extends Label
 
 const SPLASHES_FILE = "res://assets/texts/splashes.txt"
-var rng = RandomNumberGenerator.new()
-var splashes = []
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	rng.randomize()
-	var file = FileAccess.open(SPLASHES_FILE, FileAccess.READ)
-	var content = file.get_as_text()
-	for i in content.split("\n", false):
-		splashes.append(i)
-	var int_num = rng.randi_range(1, splashes.size())
-	text = splashes[int_num]
-
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+	if FileAccess.file_exists(SPLASHES_FILE):
+		var file = FileAccess.open(SPLASHES_FILE, FileAccess.READ)
+		var content = file.get_as_text()
+		var splashes = content.split("\n", false)
+		
+		if not splashes.is_empty():
+			text = splashes.pick_random()
