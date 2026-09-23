d48e02bd9472e67054b0bd3862d9641ab344a1e2
Mimrpim <mimrpim@gmail.com>
2026-09-23 16:53:58 +0200

    finished animation of stzart story 2

--- scenes/story_02/00_house_story.gd
+++ scenes/story_02/00_house_story.gd
@@ -1,11 +1,58 @@
 extends Node2D
 
 @onready var thief_animated_sprite_2d: AnimatedSprite2D = $Thief/AnimatedSprite2D
+@onready var thief: CharacterBody2D = $Thief
+@onready var thief_2: CharacterBody2D = $Thief2
+@onready var thief_2_animated_sprite_2d: AnimatedSprite2D = $Thief2/AnimatedSprite2D
+@onready var player: CharacterBody2D = $Player
+@onready var collision_shape_2d: CollisionShape2D = $Player/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	thief_animated_sprite_2d.speed_scale = 1.5
 	thief_animated_sprite_2d.animation = "left"
+	thief.hide()
+	thief_2_animated_sprite_2d.speed_scale = 1.5
+	thief_2_animated_sprite_2d.animation = "left"
+	thief_2.hide()
+	await get_tree().create_timer(5).timeout
+	thief.show()
+	thief.direction = -1
+	await get_tree().create_timer(0.3).timeout
+	thief_2.show()
+	thief_2.direction = -1
+	await get_tree().create_timer(0.15).timeout
+	thief.jump()
+	await get_tree().create_timer(0.4).timeout
+	thief_2.jump()
+	await get_tree().create_timer(0.1).timeout
+	thief.direction = 0
+	
+	thief_animated_sprite_2d.speed_scale = 1
+	thief_animated_sprite_2d.animation = "default"
+	await get_tree().create_timer(0.2).timeout
+	thief_2.direction = 0
+
+	thief_2_animated_sprite_2d.speed_scale = 1
+	thief_2_animated_sprite_2d.animation = "default"
+	
+	await get_tree().create_timer(0.05).timeout
+	player.reparent(thief_2)
+	thief_2.reparent(thief)
+	collision_shape_2d.queue_free()
+	player.position = Vector2(0, player.position.y - 10)
+	await get_tree().create_timer(0.3).timeout
+	thief_animated_sprite_2d.speed_scale = 1.5
+	thief_animated_sprite_2d.animation = "right"
+	thief_2_animated_sprite_2d.speed_scale = 1.5
+	thief_2_animated_sprite_2d.animation = "right"
+	thief.direction = 1
+	await get_tree().create_timer(0.9).timeout
+	thief_2.hide()
+	await get_tree().create_timer(0.1).timeout
+	thief.hide()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
