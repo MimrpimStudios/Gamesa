e18a6115522cdddd7f93d785815b7bfaf4a84e54
Mimrpim <mimrpim@gmail.com>
2026-09-23 16:57:30 +0200

    finished first anim of sotry 2

--- scenes/story_02/00_house_story.gd
+++ scenes/story_02/00_house_story.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var thief_2_animated_sprite_2d: AnimatedSprite2D = $Thief2/AnimatedSprite2D
 @onready var player: CharacterBody2D = $Player
 @onready var collision_shape_2d: CollisionShape2D = $Player/CollisionShape2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -41,7 +42,7 @@ func _ready() -> void:
 	player.reparent(thief_2)
 	thief_2.reparent(thief)
 	collision_shape_2d.queue_free()
-	player.position = Vector2(0, player.position.y - 10)
+	player.position = Vector2(0, player.position.y - 7)
 	await get_tree().create_timer(0.3).timeout
 	thief_animated_sprite_2d.speed_scale = 1.5
 	thief_animated_sprite_2d.animation = "right"
@@ -52,6 +53,8 @@ func _ready() -> void:
 	thief_2.hide()
 	await get_tree().create_timer(0.1).timeout
 	thief.hide()
+	await get_tree().create_timer(1).timeout
+	animation_player.play("fade")
 
 
 
