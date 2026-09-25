ed3b388d2f0d448746d967d0cc255b6b11461582
Mimrpim <mimrpim@gmail.com>
2026-09-25 14:32:03 +0200

    added chair texture to the basement scene

--- scenes/story_02/basement/chair_mechanic.gd
+++ scenes/story_02/basement/chair_mechanic.gd
@@ -2,6 +2,11 @@ extends Node
 
 @onready var player: CharacterBody2D = $"../Player"
 
+@onready var player_animated_sprite_2d: AnimatedSprite2D = $"../Player/AnimatedSprite2D"
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+const PLAYER_ROPE_CHAIR_BROKEN = preload("uid://c3y05odf08dpn")
+
 var jump_count: int = 0
 func _ready() -> void:
 	await get_tree().process_frame
@@ -9,7 +14,10 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if not global_var.player_movement:
-		player.position = Vector2(8.0, -3.0)
+		player.hide()
+		player.position = Vector2(8.0, -10.0)
+		player_animated_sprite_2d.animation = "sitting_couch"
+		
 
 func _input(_event: InputEvent) -> void:
 	if not jump_count == 5:
@@ -18,3 +26,5 @@ func _input(_event: InputEvent) -> void:
 	else:
 		await get_tree().process_frame
 		global_var.player_movement = true
+		player.show()
+		sprite_2d.set_texture(PLAYER_ROPE_CHAIR_BROKEN)
